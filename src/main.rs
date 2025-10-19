25cf3bd0f2bc3aa154130f888d9c2a743dd28b74
Or Kwitzel <orkv1234@gmail.com>
2025-10-19 05:36:36 +0300

    gets avaliable arch updates

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use std::{process::Command, thread};
+use std::{panic, process::Command, thread};
 
 use spinoff::{Color, Spinner, spinners};
 
 fn main() {
+    let avaliable_updates = get_available_arch_updates();
+    eprintln!("{:#?}", avaliable_updates);
+
     let mut spinner = Spinner::new(spinners::Dots, "Downloading updates...", Color::Blue);
 
     let handle1 = thread::spawn(|| {
@@ -23,4 +26,44 @@ fn main() {
     handle2.join().unwrap();
 
     spinner.success("Done!");
+}
+
+#[derive(Debug)]
+struct PacmanPackageData {
+    name: String,
+    current_ver: String,
+    new_ver: String,
+}
+
+fn get_available_arch_updates() -> Vec<PacmanPackageData> {
+    let output = Command::new("checkupdates")
+        .output()
+        .expect("Something went wrong while searching for Arch updates");
+
+    if !output.status.success() {
+        panic!("Something went wrong while getting available pacman updates");
+    }
+
+    let stdout = match str::from_utf8(&output.stdout) {
+        Ok(s) => s,
+        Err(e) => panic!("Command output was not valid UTF-8: {}", e),
+    };
+
+    let mut packages = Vec::new();
+
+    for line in stdout.lines() {
+        let parts: Vec<&str> = line.split_whitespace().collect();
+
+        if parts.len() == 3 {
+            let package_data = PacmanPackageData {
+                name: parts[0].to_string(),
+                current_ver: parts[1].to_string(),
+                new_ver: parts[2].to_string(),
+            };
+
+            packages.push(package_data);
+        }
+    }
+
+    packages
 }
